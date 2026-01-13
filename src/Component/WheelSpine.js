07a607cfa7f2917047b056bc41b2c37d0cda540f
Hrishali7 <hrishali.d@zvky.com>
2026-01-13 19:08:11 +0530

    updated logic

--- src/Component/WheelSpine.js
+++ src/Component/WheelSpine.js
@@ -1,12 +1,44 @@
-
 import { useEffect, useRef } from "react";
 import * as PIXI from "pixi.js";
 import { Spine } from "pixi-spine";
 import pointerImage from "../assets/wheel1.png";
+import gsap from "gsap";
 
 function WheelSpine() {
+
   const spineRef = useRef(null);
-    
+  let wheelRef = useRef(null);
+
+  function rotationWheel() {
+    console.log("rotation wheel called");
+
+    const wheelBone = wheelRef?.current.skeleton.findBone("wheel");
+    const fullSpins = 2;
+    const desiredIndex = 5;
+    console.log("targetRotation", wheelBone);
+  console.log(wheelBone.rotation);
+     if(wheelBone){
+       const targetRotation =  (fullSpins * 360) + (22.5 * (desiredIndex - 1));   
+        
+        gsap.to(wheelBone, {
+           rotation: targetRotation,
+           duration: 4,         
+           ease: "power4.out",   
+           onUpdate: () => {
+           },
+
+           onComplete: () => {
+          console.log(wheelBone.rotation);
+             wheelBone.rotation = 22.5 * (desiredIndex - 1);
+             wheelRef.current.state.setAnimation(
+  0,
+  "golden/win_wedge_with_special_effect_intro",
+  false
+);
+     }
+   });
+     }
+  }
   useEffect(() => {
     const app = new PIXI.Application({ resizeTo: window, transparent: true });
     window.__PIXI_APP__ = app;
@@ -17,34 +49,16 @@ function WheelSpine() {
     container.y = app.screen.height / 2;
     app.stage.addChild(container);
 
-    let speed = 0;
-    let hasPlayedWin = false;
-    const TOTAL_SLOTS = 16;
-    const SEGMENT_ANGLE = (Math.PI * 2) / TOTAL_SLOTS;
-    let desiredIndex = 5;
-    let targetAngle = null;
-    let hasSnapped = false;
-    
 
     PIXI.Assets.load("/Assets/Normal_wheel.json").then((spineResource) => {
       const skeleton = new Spine(spineResource.spineData);
       skeleton.scale.set(0.4);
       container.addChild(skeleton);
+      wheelRef.current = skeleton;
 
       skeleton.state.setAnimation(0, "golden/wheel_intro", false);
       const wheelBone = skeleton.skeleton.findBone("wheel");
 
-          function setDesiredStop() {
-          desiredIndex = 7;
-          const anglePerSlot = SEGMENT_ANGLE;
-          const currentRotation = wheelBone.rotation ;
-          const fullSpins = 2; 
-targetAngle = wheelBone.rotation + fullSpins * 2 * Math.PI + desiredIndex * SEGMENT_ANGLE;
-
-         hasSnapped = false;
-         speed = Math.PI * 2;
-         
-}
       const pointerSprite = new PIXI.Sprite(PIXI.Texture.from(pointerImage));
       pointerSprite.anchor.set(0.5,1);
       pointerSprite.scale.set(0.5);
@@ -62,6 +76,8 @@ targetAngle = wheelBone.rotation + fullSpins * 2 * Math.PI + desiredIndex * SEGM
       buttonGraphics.endFill();
       buttonGraphics.interactive = true;
       buttonGraphics.buttonMode = true;
+      buttonGraphics.eventMode= "static";
+      buttonGraphics.cursor ="pointer";
       buttonContainer.addChild(buttonGraphics);
 
         const buttonText = new PIXI.Text("SPIN", {
@@ -76,13 +92,11 @@ targetAngle = wheelBone.rotation + fullSpins * 2 * Math.PI + desiredIndex * SEGM
       buttonText.y = 25;
       buttonContainer.addChild(buttonText);
 
-        buttonGraphics.on("pointerdown", () => {
-        if (!wheelBone) return;
-        hasPlayedWin = false;
-        hasSnapped = false;
-        setDesiredStop(7);
-      });
-
+   buttonGraphics.on("pointerdown", () => {
+    console.log("button clicked");
+    rotationWheel()
+    
+  });
         const numberStyle = new PIXI.TextStyle({
         fontFamily: "Arial",
         fontSize: 40,
@@ -90,7 +104,6 @@ targetAngle = wheelBone.rotation + fullSpins * 2 * Math.PI + desiredIndex * SEGM
         fontWeight: "bold",
         align: "center",
       });
- 
         for (let i = 1; i <= 16; i++) {
         const slotName = "place_holder" + (i === 1 ? "" : i);
         const slot = skeleton.skeleton.findSlot(slotName);
@@ -100,38 +113,15 @@ targetAngle = wheelBone.rotation + fullSpins * 2 * Math.PI + desiredIndex * SEGM
         numberText.anchor.set(0.5);
         slot.currentSprite.addChild(numberText);
       }
-
         skeleton.state.addListener({
         complete: (entry) => {
         const name = entry.animation.name;
-        
             if (name === "golden/win_wedge_with_special_effect_intro") {
             skeleton.state.setAnimation(0,"golden/win_wedge_with_special_effect_loop",true);
           }
         },
       });
 
-         app.ticker.add(() => {
-         if (speed > 0) {
-          wheelBone.rotation -= speed;
-          speed -= 0.007;
-
-    if (desiredIndex !== null && !hasSnapped && speed < 0.05 ){
-        const delta = wheelBone.rotation - targetAngle;
-            if (Math.abs(delta) < 0.01) {
-                wheelBone.rotation = targetAngle;
-                speed = 0;
-                hasSnapped = true;
-    }
-    if (!hasPlayedWin ) {
-              hasPlayedWin = true;
-              skeleton.state.setAnimation(0, "golden/win_wedge_with_special_effect_intro", false );
-            }
-          }
-        }
-        
-        skeleton.update(app.ticker.deltaMS / 1000);
-      });
     });
 
     return () => {
@@ -143,6 +133,65 @@ targetAngle = wheelBone.rotation + fullSpins * 2 * Math.PI + desiredIndex * SEGM
 }
 
 export default WheelSpine;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
